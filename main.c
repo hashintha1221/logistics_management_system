eb89df2d6d9388b3016f92c85fc2ecd69d9eb30c
hashintha1221 <hashinthahashintha386@gmail.com>
2025-10-16 08:06:58 +0530

    Added city management feature (add city)

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_CITIES 30
+
+char cities[MAX_CITIES][50];
+int cityCount = 0;
 
 
 //function declarations
@@ -48,9 +52,32 @@ int main()
     }
 
 }
- void addCity() {
-    printf("City added!\n");
+
+   //city function
+void addCity() {
+    if (cityCount >= MAX_CITIES) {
+        printf("City limit reached!\n");
+        return;
+    }
+    char name[50];
+    printf("Enter city name: ");
+    scanf("%s", name);
+
+    // Check for duplicates
+    for (int i = 0; i < cityCount; i++) {
+        if (strcmp(cities[i], name) == 0) {
+            printf("City already exists!\n");
+            return;
+        }
+    }
+
+    strcpy(cities[cityCount++], name);
+    printf("City added successfully!\n");
 }
+
+
+
+
 
 void listCities() {
     printf("List of cities\n");
